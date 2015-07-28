0e0c3e913b086976a4662cb654242b88f5ffdadd
Jan Hrcek <jhrcek@redhat.com>
2015-07-28 12:44:49 +0200

    extact common fs path

--- prepare.sh
+++ prepare.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+
+ANKI_DB=~/Dropbox/Reference/Anki/User\ 1/collection.anki2
 echo "Removing old backup"
-rm ~/Dropbox/Reference/Anki/User\ 1/collection.anki2_bck
+rm "${ANKI_DB}_bck"
 echo "Creating new backup"
-cp ~/Dropbox/Reference/Anki/User\ 1/collection.anki2 ~/Dropbox/Reference/Anki/User\ 1/collection.anki2_bck
+cp "$ANKI_DB" "${ANKI_DB}_bck"
 echo "Copying collection.anki2 to local dir for further processing"
-cp ~/Dropbox/Reference/Anki/User\ 1/collection.anki2 .
+cp "$ANKI_DB" .
 echo "Checking database consistency (Haskell)"
 runghc anki.hs
 echo "Checking database consistency (Java)"
