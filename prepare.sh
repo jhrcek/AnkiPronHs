78b03769baa783356a8d56043fee265baccc8def
Jan Hrcek <jhrcek@redhat.com>
2018-08-23 13:15:10 +0200

    Simplify prepare and finalize scripts

--- prepare.sh
+++ prepare.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 ANKI_DB=~/Dropbox/Reference/Anki/User\ 1/collection.anki2
-echo "Removing old backup"
-rm "${ANKI_DB}_bck"
-echo "Creating new backup"
+echo "Creating backup copy of Anki DB"
 cp "$ANKI_DB" "${ANKI_DB}_bck"
 echo "Copying collection.anki2 to local dir for further processing"
 cp "$ANKI_DB" .
-echo "Checking database consistency (Haskell)"
 stack exec anki-exe
-
